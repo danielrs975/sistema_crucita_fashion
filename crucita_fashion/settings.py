aad6fe575d2807379e42134a4ce4e0ccdda787ec
Daniel <danielrs9705@gmail.com>
2018-12-25 18:22:53 -0400

    Se cambia la configuracion de static root en settings

--- crucita_fashion/settings.py
+++ crucita_fashion/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, '.staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
 os.makedirs(STATIC_TMP, exist_ok=True)
