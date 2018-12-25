fa74a88c3e1303dbf913a1ed957dbf322628e6de
Daniel <danielrs9705@gmail.com>
2018-12-25 16:56:43 -0400

    Se agrega configuracion de la ubicacion de los archivos estaticos

--- crucita_fashion/settings.py
+++ crucita_fashion/settings.py
@@ -119,8 +119,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_ROOT = os.path.join(BASE_DIR, '.staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
