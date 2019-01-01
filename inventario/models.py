2d719f684627591520468f2b2477e8748d0cb8cc
Daniel <danielrs9705@gmail.com>
2019-01-01 16:05:57 -0400

    Se anade los modelos necesario para inventario

--- inventario/models.py
+++ inventario/models.py
@@ -5,3 +5,22 @@ aplicacion
 from django.db import models # pylint: disable=unused-import
 
 # Create your models here.
+
+class Categoria(models.Model):
+    '''
+    Creacion de la tabla categoria que 
+    dividira los productos en distintos tipos
+    '''
+    nombre = models.CharField(unique=True)
+
+class Producto(models.Model):
+    '''
+    Creacion de la tabla Producto, esta contendra
+    todos los productos que esten disponibles
+    '''
+    codigo = models.CharField(unique=True)
+    cantidad = models.PositiveIntegerField()
+    costo = models.FloatField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    talla = models.CharField(null=True)
+    foto = models.ImageField()
