29f5a9f9983b6cfb664e2d3952a87326c79d52fe
Daniel <danielrs9705@gmail.com>
2019-01-01 16:38:04 -0400

    Se anade la primera migracion a la base

--- inventario/models.py
+++ inventario/models.py
@@ -11,16 +11,16 @@ class Categoria(models.Model):
     Creacion de la tabla categoria que 
     dividira los productos en distintos tipos
     '''
-    nombre = models.CharField(unique=True)
+    nombre = models.CharField(unique=True, max_length=100)
 
 class Producto(models.Model):
     '''
     Creacion de la tabla Producto, esta contendra
     todos los productos que esten disponibles
     '''
-    codigo = models.CharField(unique=True)
+    codigo = models.CharField(unique=True, max_length=500)
     cantidad = models.PositiveIntegerField()
     costo = models.FloatField()
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    talla = models.CharField(null=True)
-    foto = models.ImageField()
+    talla = models.CharField(null=True, max_length=100)
+    foto = models.ImageField(null=True)
