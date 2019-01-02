5311fbb9c57625246a41b6fa4343eeb2885608d8
Daniel <danielrs9705@gmail.com>
2019-01-02 01:31:41 -0400

    Se anade las vistas para crear, buscar, eliminar y actualizar

--- inventario/views.py
+++ inventario/views.py
@@ -3,5 +3,30 @@ Script que contendra las vistas de este
 modulo
 '''
 from django.shortcuts import render # pylint: disable=unused-import
+from django.http import Http404
+from rest_framework import generics
+from inventario.models import Producto
+from inventario.serializers import ProductoSerializer
 
 # Create your views here.
+class ProductoCrearView(generics.CreateAPIView):
+    '''
+    Vista que se encarga de la creacion de un
+    nuevo producto para anadir al sistema
+    '''
+    serializer_class = ProductoSerializer
+
+class ProductoDetallesView(generics.RetrieveUpdateDestroyAPIView):
+    '''
+    Vista que permite modificar, ver o eliminar
+    la informacion de un producto dentro del sistema
+    '''
+    serializer_class = ProductoSerializer
+
+class ProductoBuscarView(generics.ListAPIView):
+    '''
+    Vista que permite buscar uno o varios productos
+    en el sistema
+    '''
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
