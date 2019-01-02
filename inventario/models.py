1b55bc0569c327c4fd594eb1cc5f0c0a7efb2691
Daniel <danielrs9705@gmail.com>
2019-01-01 20:24:25 -0400

    Incluye unas variables con las tallas de ropa y zapatos

--- inventario/models.py
+++ inventario/models.py
@@ -4,7 +4,37 @@ aplicacion
 '''
 from django.db import models # pylint: disable=unused-import
 
+TALLA_ROPA = [
+    "XXS",
+    "XS",
+    "S",
+    "M",
+    "L",
+    "XL",
+]
+
+TALLA_ZAPATOS = [
+    "5",
+    "5.5",
+    "6",
+    "6.5",
+    "7",
+    "7.5",
+    "8",
+    "8.5",
+    "9",
+    "9.5",
+    "10",
+    "10.5",
+    "11",
+    "11.5",
+    "12",
+    "12.5",
+    "13"
+]
+
 # Create your models here.
+
 
 class Categoria(models.Model):
     '''
