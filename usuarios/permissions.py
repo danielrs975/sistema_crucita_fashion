f1716d97a523f4a8614f11a41e96f97e550f06bf
Daniel <danielrs9705@gmail.com>
2019-01-03 17:44:24 -0400

    Anade el permiso no esta autenticado

--- usuarios/permissions.py
+++ usuarios/permissions.py
@@ -19,6 +19,26 @@ class EsSuperUsuarioOAdministrador(permissions.BasePermission):
         """
         usuario = request.user
         grupo = usuario.grupo
-        if grupo.name in ["SuperUsuario", "Administrador"]:
-            return True
-        return False
+        return grupo.name in ["SuperUsuario", "Administrador"]
+    
+class IsNotAuthenticated(permissions.BasePermission):
+    """
+    This implement the following permission:
+    Only not authenticated user can use the views
+    that use this class
+
+    Esto implementa el siguiente permiso:
+    Solo los usuarios no autenticados pueden usar
+    las vistas que usan esta clase
+    """
+
+    def has_permission(self, request, view):
+        """
+        This method verify that the user
+        who uses the view is not authenticated
+
+        Este metodo verifica que el usuario
+        que usa la vista este no autenticado
+        """
+        usuario = request.user
+        return str(usuario) == "AnonymousUser"
