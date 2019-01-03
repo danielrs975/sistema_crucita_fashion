c8c1233a228af42847ad463e45253e87ec05c3a1
Daniel <danielrs9705@gmail.com>
2019-01-03 17:45:01 -0400

    Se conecta la vista con un url

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -10,6 +10,7 @@ app_name = "inventario"
 
 urlpatterns = [
     path('usuarios/crear', views.UsuarioCrearViewSuperUsuario.as_view(), name="crear"),
+    path('usuarios/registrarse', views.UsuarioRegistroView.as_view(), name="registro")
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
