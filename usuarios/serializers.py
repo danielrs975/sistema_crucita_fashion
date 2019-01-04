4aa2605b04d54ceb604a12bdc770ada8614bb598
Daniel <danielrs9705@gmail.com>
2019-01-04 10:53:51 -0400

    Se crea el serializer para el registro

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -3,7 +3,7 @@ Script que contiene los serializers
 para el modelo Usuario
 """
 from rest_framework import serializers
-from .models import Usuario
+from .models import Usuario, Group
 
 class UsuarioSerializer(serializers.ModelSerializer):
     """
@@ -22,6 +22,35 @@ class UsuarioSerializer(serializers.ModelSerializer):
             if grupo.usuario_set.count() == 1:
                 raise serializers.ValidationError("No puede haber mas SuperUsuarios")
         return grupo
+
+    class Meta:
+        model = Usuario
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'grupo',
+            'password'
+        )
+
+class RegistroSerializer(serializers.ModelSerializer):
+    """
+    Clase que contiene el serializer que sera usado
+    para el registro
+    """
+
+    def validate_grupo(self, grupo): # pylint: disable=no-self-use
+        """
+        Validator para el campo grupo: valida
+        que el unico grupo al cual puede pertenecerse
+        si usa el registro es cliente
+        """
+        message = "Este serializer no admite la escogencia de un grupo"
+        if grupo is not None and grupo.name in ["SuperUsuario", "Administrador", "Vendedor"]:
+            raise serializers.ValidationError(message)
+
+        return Group.objects.get(name="Vendedor")
 
     class Meta:
         model = Usuario
