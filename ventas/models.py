197ce44763067d63aae6afde7f4da295dc1f47fd
David Rod <dave00dark@gmail.com>
2019-01-04 19:30:29 -0400

    Añadido los modelos de las ventas

--- ventas/models.py
+++ ventas/models.py
@@ -3,5 +3,13 @@ Script que contendra los modelos de la base
 de datos para las ventas
 '''
 from django.db import models # pylint: disable=unused-import
+from inventario.models import Producto
 
 # Create your models here.
+
+class Venta(models.Model):
+	producto = models.ManyToManyField(Producto)
+	codigo = models.CharField(unique=True, max_length=500)
+	costo_total = models.FloatField()
+	fecha = models.DateField()
+	hora = models.DateTimeField()
