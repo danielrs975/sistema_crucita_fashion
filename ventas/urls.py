af77d16c75a4d968bd8537116df6c8855270a431
David Rod <dave00dark@gmail.com>
2019-01-13 16:56:06 -0400

    Pequeñas modificaciones

--- ventas/urls.py
+++ ventas/urls.py
@@ -1,3 +1,7 @@
+'''
+Script con las urls asociadas a esta modulo
+
+'''
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from ventas import views
@@ -6,7 +10,7 @@ app_name='ventas'
 
 urlpatterns = [
 	path('', views.VentasBuscarView.as_view()),
-	path('crear/', views.VentasCrearView.as_view()),
+	path('crear', views.VentasCrearView.as_view()),
 	path('<int:pk>', views.VentasDetallesView.as_view()),
 ]
 
