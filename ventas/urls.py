3c81edc51373908061b8523ea29a03bee5ac7efe
David Rod <dave00dark@gmail.com>
2019-01-19 14:39:02 -0400

    Cambiado los path de ventas

--- ventas/urls.py
+++ ventas/urls.py
@@ -6,12 +6,12 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from ventas import views
 
-app_name='ventas'
+app_name = 'ventas'
 
 urlpatterns = [
-	path('', views.VentasBuscarView.as_view()),
-	path('crear', views.VentasCrearView.as_view()),
-	path('<int:pk>', views.VentasDetallesView.as_view()),
+    path('', views.VentasBuscarView.as_view()),
+    path('crear', views.VentasCrearView.as_view()),
+    path('<int:pk>', views.VentasDetallesView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
