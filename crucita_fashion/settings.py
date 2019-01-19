aac13e90b95a97fdb5bde2e88b52d30e1b30f6a3
Daniel <danielrs9705@gmail.com>
2019-01-18 21:21:59 -0400

    Instala django-heroku

--- crucita_fashion/settings.py
+++ crucita_fashion/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -157,3 +158,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
+
+django_heroku.settings(locals())
