747a2653b79729c42cc94a149127e5b3b34a14b7
Daniel <danielrs9705@gmail.com>
2019-01-18 21:48:24 -0400

    Se configura para hacer deploy en heroku

--- crucita_fashion/settings.py
+++ crucita_fashion/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -157,5 +156,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
-
-django_heroku.settings(locals())
