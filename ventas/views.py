d159ea3f5cd0408daf1e2f96fd55dde47be4dc33
David Rod <dave00dark@gmail.com>
2019-01-19 14:39:02 -0400

    Cambiado los path de ventas

--- ventas/views.py
+++ ventas/views.py
@@ -10,24 +10,24 @@ from ventas.serializers import VentasSerializer
 # Create your views here.
 
 class VentasCrearView(generics.CreateAPIView):
-	'''
-	Vista que se encarga de la creacion de una venta
-	para ser anadida al sistema
-	'''
-	queryset = Ventas.objects.all()
-	serializer_class = VentasSerializer
+    '''
+    Vista que se encarga de la creacion de una venta
+    para ser anadida al sistema
+    '''
+    queryset = Ventas.objects.all()
+    serializer_class = VentasSerializer
 
-class VentasDetallesView(generics.RetrieveUpdateDestroyAPIView):
-	'''
-	Vista que se encarga de ver, modificar o eliminar una venta
-	'''
-	queryset = Ventas.objects.all()
-	serializer_class = VentasSerializer
+class VentasDetallesView(generics.RetrieveUpdateDestroyAPIView): # pylint: disable=too-many-ancestors
+    '''
+    Vista que se encarga de ver, modificar o eliminar una venta
+    '''
+    queryset = Ventas.objects.all()
+    serializer_class = VentasSerializer
 
 class VentasBuscarView(generics.ListAPIView):
-	''' 
-	Vista que se encarga de buscar y mostrar una Venta o una lista
-	de ventas
-	'''
-	queryset = Ventas.objects.all()
-	serializer_class = VentasSerializer
+    '''
+    Vista que se encarga de buscar y mostrar una Venta o una lista
+    de ventas
+    '''
+    queryset = Ventas.objects.all()
+    serializer_class = VentasSerializer
