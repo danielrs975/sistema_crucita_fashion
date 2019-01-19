4e8b4f9d47991e59dccd84448fee02d681a4cfca
David Rod <dave00dark@gmail.com>
2019-01-19 17:49:12 -0400

    Modelos de apartados

--- apartados/models.py
+++ apartados/models.py
@@ -3,5 +3,17 @@ Script que contendra los modelos de la
 aplicacion de los apartados
 '''
 from django.db import models # pylint: disable=unused-import
+from inventario.models import Productos
+from usuarios.models import Usuario
 
 # Create your models here.
+
+class Aparatado(models.Model):
+	'''
+	Creacion de la tabla de apartados, aqui se guardan 
+	los productos que cada usuario aparte
+	'''
+	usuario = models.ForeignKey('Usuario', on_delete=PROTECT)
+	codigo = models.CharField(max_length=100, unique=True)
+	producto = models.ManyToManyField('Productos')
+	costo_total = models.FloatField()
