b88367a48a25c648f2fa908ca0e644a17c53efe7
David Rod <dave00dark@gmail.com>
2019-01-20 14:04:38 -0400

    Añadido las vistas del modulo de apartados

--- apartados/views.py
+++ apartados/views.py
@@ -2,5 +2,36 @@
 Script que contendra las vistas de este modulo
 '''
 from django.shortcuts import render # pylint: disable=unused-import
-
+from rest_framwork import generics
+from .models import Apartado
+from .serializers import AparatadoSerializer
 # Create your views here.
+
+class ApartadoCrearView(generics.CreateAPIView):
+	'''
+	Clase de vista para crear un apartado
+	'''
+	query_set = Apartado.objects.all()
+	serializer_class = AparatadoSerializer
+
+class AparatadoBorrarView(generics.RetrieveDestroyAPIView):
+	'''
+	Clase de vista para ver y eliminar un apartado
+	'''
+	query_set = Apartado.objects.all()
+	serializer_class = AparatadoSerializer
+
+class ApartadoModificarView(generics.RetrieveUpdateAPIView):
+	'''
+	Clase de vista para ver y modificar un apartado
+	'''
+	query_set = Apartado.objects.all()
+	serializer_class = AparatadoSerializer
+
+class ApartadoBuscarView(generics.ListAPIView):
+	'''
+	Clase de vista para Buscar coleccion de 
+	objetos de un apartado
+	'''
+	query_set = Apartado.objects.all()
+	serializer_class = AparatadoSerializer
